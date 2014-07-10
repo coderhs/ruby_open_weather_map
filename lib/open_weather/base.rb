49de073897b3d8f55bbe097c5d7f6e979041c31a
Genta Kaneyama <genta-kaneyama@cookpad.com>
2014-07-10 18:19:24 +0900

    Add units options

--- lib/open_weather/base.rb
+++ lib/open_weather/base.rb
@@ -24,7 +24,7 @@ module OpenWeather
     private
 
     def extract_options!(options)
-      valid_options = [:lat, :lon, :city, :country, :id]
+      valid_options = [:lat, :lon, :city, :country, :id, :units]
       options.keys.each { |k| options.delete(k) unless valid_options.include?(k) }
 
       if options[:city] || options[:country]
