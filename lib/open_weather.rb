778e9f9a77cc4fe3578996c4bedf00026966eef1
Deepak Kumar <deepakkumarnd@gmail.com>
2015-03-17 00:41:40 +0530

    cleanup, use ' for strings whereever possible, cleanup alignments

--- lib/open_weather.rb
+++ lib/open_weather.rb
@@ -1,11 +1,11 @@
 module OpenWeather
-  $LOAD_PATH<< "../lib"
+  $LOAD_PATH<< '../lib'
 
-  autoload :Base,    "open_weather/base"
-  autoload :Current, "open_weather/current"
-  autoload :Forecast, "open_weather/forecast"
-  autoload :ForecastDaily, "open_weather/forecast_daily"
-  autoload :VERSION, "open_weather/version"
+  autoload :Base,          'open_weather/base'
+  autoload :Current,       'open_weather/current'
+  autoload :Forecast,      'open_weather/forecast'
+  autoload :ForecastDaily, 'open_weather/forecast_daily'
+  autoload :VERSION,       'open_weather/version'
 
-  require "open_weather/api.rb"
+  require 'open_weather/api.rb'
 end
--- lib/open_weather/api.rb
+++ lib/open_weather/api.rb
@@ -1,19 +1,19 @@
 
 module OpenWeather
   module ClassMethods
-    #City format : Eg, Cochin,IN
-    #Useage: OpenWeather::Current.city('Cochin,In')
+    # City format : Eg, Cochin,IN
+    # Useage: OpenWeather::Current.city('Cochin,In')
     def city(city, options = {})
       new(options.merge(city: city)).retrive
     end
 
-    #City Id, an integer value. Eg, 2172797
-    #Useage: OpenWeather::Current.city_id(2172797)
+    # City Id, an integer value. Eg, 2172797
+    # Useage: OpenWeather::Current.city_id(2172797)
     def city_id(id, options = {})
       new(options.merge(id: id)).retrive
     end
 
-    #City Geographics Cordingates : Eg, lat 35 lon 139
+    # City Geographics Cordingates : Eg, lat 35 lon 139
     def geocode(lat, lon, options = {})
       new(options.merge(lat: lat, lon: lon)).retrive
     end
