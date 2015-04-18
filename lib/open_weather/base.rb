0e26161ee0c366ef73f509fad57073c8b7ab712c
Deepak Kumar <deepakkumarnd@gmail.com>
2015-04-18 15:55:24 +0530

    use ' instead of " for fixed string literals

--- lib/open_weather/base.rb
+++ lib/open_weather/base.rb
@@ -6,15 +6,15 @@ module OpenWeather
 
     attr_reader :url, :options, :weather_info, :status, :message
 
-    def initialize url, options
-      @status = false
-      @url = url
+    def initialize(url, options)
+      @status  = false
+      @url     = url
       @options = extract_options!(options)
     end
 
     def retrive
-      @response = send_request(self) unless @options.empty?
-      parse_response if @response
+      response = send_request unless @options.empty?
+      parse_response(response)
     end
 
     def success?
@@ -24,7 +24,9 @@ module OpenWeather
     private
 
     def extract_options!(options)
-      valid_options = [:lat, :lon, :city, :country, :id, :units, :cnt, :APPID, :lang]
+      valid_options = [ :id, :lat, :lon, :cnt, :city, :lang, :units, :APPID,
+        :country]
+
       options.keys.each { |k| options.delete(k) unless valid_options.include?(k) }
 
       if options[:city] || options[:country]
@@ -36,16 +38,17 @@ module OpenWeather
       options
     end
 
-    def parse_response
-      @weather_info = JSON.parse(@response)
-      @status = @weather_info["cod"]
-      @message = @weather_info["message"] unless @status
+    def parse_response(response)
+      return if response.nil?
+      @weather_info = JSON.parse(response)
+      @status       = @weather_info['cod']
+      @message      = @weather_info['message'] unless @status
       @weather_info
     end
 
-    def send_request(request)
-      uri = URI(request.url)
-      uri.query = URI.encode_www_form(request.options)
+    def send_request
+      uri = URI(url)
+      uri.query = URI.encode_www_form(options)
       Net::HTTP.get(uri)
     end
   end
--- spec/open_weather/version_spec.rb
+++ spec/open_weather/version_spec.rb
@@ -1,5 +1,5 @@
-describe "Version" do
-  it "should be version 0.10.0" do
-    OpenWeather::VERSION.should == "0.10.0"
+describe 'Version' do
+  it 'should be version 0.10.0' do
+    OpenWeather::VERSION.should == '0.10.0'
   end
 end
