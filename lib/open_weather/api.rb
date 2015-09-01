9eeb4462e6b89b41f6e7f891b01bb2e04c24e0fa
Jesse Emond <emond.jesse@gmail.com>
2015-09-01 12:52:39 -0400

    fix retrieve and usage typos

--- lib/open_weather/api.rb
+++ lib/open_weather/api.rb
@@ -2,20 +2,20 @@
 module OpenWeather
   module ClassMethods
     # City format : Eg, Cochin,IN
-    # Useage: OpenWeather::Current.city('Cochin,In')
+    # Usage: OpenWeather::Current.city('Cochin,In')
     def city(city, options = {})
-      new(options.merge(city: city)).retrive
+      new(options.merge(city: city)).retrieve
     end
 
     # City Id, an integer value. Eg, 2172797
-    # Useage: OpenWeather::Current.city_id(2172797)
+    # Usage: OpenWeather::Current.city_id(2172797)
     def city_id(id, options = {})
-      new(options.merge(id: id)).retrive
+      new(options.merge(id: id)).retrieve
     end
 
     # City Geographics Cordingates : Eg, lat 35 lon 139
     def geocode(lat, lon, options = {})
-      new(options.merge(lat: lat, lon: lon)).retrive
+      new(options.merge(lat: lat, lon: lon)).retrieve
     end
   end
 
