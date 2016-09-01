65efcaccb445a79efe9bf12a300141a4f4898d17
Phitherek_ <phitherek@gmail.com>
2016-09-01 15:47:23 +0200

    Add poorly documented find endpoint with like and accurate options

--- lib/open_weather.rb
+++ lib/open_weather.rb
@@ -5,6 +5,7 @@ module OpenWeather
   autoload :Current,       'open_weather/current'
   autoload :Forecast,      'open_weather/forecast'
   autoload :ForecastDaily, 'open_weather/forecast_daily'
+  autoload :Find,          'open_weather/find'
   autoload :VERSION,       'open_weather/version'
 
   require 'open_weather/api.rb'
---
+++ lib/open_weather/find.rb
@@ -0,0 +1,15 @@
+module OpenWeather
+  class Find < Base
+    def initialize(options = {})
+      super('http://api.openweathermap.org/data/2.5/find', options)
+    end
+
+    def self.like(q, options = {})
+      new(options.merge!(q: q, type: 'like')).retrieve
+    end
+
+    def self.accurate(q, options = {})
+      new(options.merge!(q: q, type: 'accurate')).retrieve
+    end
+  end
+end
